53ae2254c6db889df08e2a2ccee614375fa3d94c
lobodol <grobodol@gmail.com>
2018-10-22 16:08:01 +0200

    Fix vertical alignments

--- imu.ino
+++ imu.ino
@@ -1,13 +1,13 @@
 //----------------------------------------------------------------------------------------------------------------------
 #include <Wire.h>
 //----------------------------------------------------------------------------------------------------------------------
-#define YAW      0
-#define PITCH    1
-#define ROLL     2
+#define YAW   0
+#define PITCH 1
+#define ROLL  2
 
-#define X           0     // X axis
-#define Y           1     // Y axis
-#define Z           2     // Z axis
+#define X     0     // X axis
+#define Y     1     // Y axis
+#define Z     2     // Z axis
 
 #define MPU_ADDRESS 0x68  // I2C address of the MPU-6050
 #define FREQ        250   // Sampling frequency
